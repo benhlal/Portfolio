1bbf85cc946345bea238b98d9d31209485587c61
benhlal <benhlalyounes@gmail.com>
2021-08-15 23:02:35 +0200

    updates on portfolio resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -88,27 +88,6 @@ function Projects() {
                         />
                     </Col>
                 </Row>
-                <h1 className="project-heading">
-                    My Recent <strong className="purple">Blog </strong> Posts
-                </h1>
-                <p style={{color: "white"}}>Do give a read to some of my blogs</p>
-                <Row style={{justifyContent: "center"}}>
-                    <Col md={4} className="blog-card">
-                        <BlogCard
-                            imgPath={slideImages.ch}
-                            title="Cracking Interview"
-                            site=""
-                        />
-                    </Col>
-                    <Col md={4} className="blog-card">
-                        <BlogCard
-                            imgPath={slideImages.ch}
-                            link=""
-                            title="Plant AI"
-                            site="medium.com"
-                        />
-                    </Col>
-                </Row>
             </Container>
         </Container>
     );
