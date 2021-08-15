121792d8aeb62355baf035dd09cdfb59bfea23a3
benhlal <benhlalyounes@gmail.com>
2021-08-15 23:23:22 +0200

    updates on portfolio resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
-import BlogCard from "./BlogsCards";
 import Particle from "../Particle";
 
 import slideImages from "../../Assets/Projects/slideImages.json";
